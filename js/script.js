68c1ecbe2cd2acf95851daeffcc6ccd14940203c
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-18 00:09:26 +0600

    all ok website is working and done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("all ok");
+let loddatas=()=>{
+    let url=`https://jsonplaceholder.typicode.com/users`
+    fetch(url)
+    .then(res=>res.json())
+    .then((data)=>showData(data))
+}
+
+let showData=(phones)=>{
+
+    let get1=document.getElementById("container")
+console.log(phones);
+    
+
+    for(let mahedi of phones ){
+        
+
+    let create=document.createElement("div")
+    
+
+
+    create.innerHTML=   `
+    <h1>${mahedi.id}</h1>
+    <h1>${mahedi.name}</h1>
+    <h1>${mahedi.username}</h1>
+    <h1>${mahedi.email}</h1>
+
+    
+    
+    `;
+    get1.appendChild(create)
+
+    }
+
+
+}
+
+
+loddatas()
